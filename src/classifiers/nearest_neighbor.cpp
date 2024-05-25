88477b23e4803bf55501248ca3d0236341568040
Anshul Gupta <ansg191@anshulg.com>
2024-05-24 18:31:28 -0700

    Add `nearest_neighbor` classifier

---
+++ src/classifiers/nearest_neighbor.cpp
@@ -0,0 +1,92 @@
+//
+// Created by Anshul Gupta on 5/24/24.
+//
+
+#include <cassert>
+
+#include "nearest_neighbor.h"
+
+namespace classifiers {
+
+nearest_neighbor::nearest_neighbor(size_t k) : k(k)
+{
+	assert(k > 0);
+	assert(k % 2 == 1); // k must be odd
+}
+
+void nearest_neighbor::train(data_view view)
+{
+	// No training optimization is performed in this classifier.
+	// It could be optimized by using data structures like k-d trees or vp-trees.
+	training_data.emplace(view);
+}
+
+double nearest_neighbor::test(data_view view) const
+{
+	// If the training data is not available, throw an exception.
+	if (!training_data.has_value()) throw std::runtime_error("Training data is not available.");
+
+	// Check that the number of features in the test data is the same as the number of features
+	// in the training data.
+	if (view.get_features().size() != training_data->get_features().size())
+		throw std::runtime_error("The number of features in the test data is not the same "
+		                         "as the number of features in the training data.");
+
+	// Initialize the number of correct predictions to 0.
+	size_t correct_predictions = 0;
+
+	// For each data point in the test data, find the k nearest neighbors in the training data.
+	for (size_t i = 0; i < view.size(); ++i) {
+		uint8_t predicted_label = predict(view, i);
+		uint8_t actual_label = view.label(i);
+
+		if (predicted_label == actual_label) correct_predictions++;
+	}
+
+	// Return the accuracy of the classifier.
+	return static_cast<double>(correct_predictions) / static_cast<double>(view.size());
+}
+uint8_t nearest_neighbor::predict(const data_view &view, size_t index) const
+{
+	// k-nearest distances
+	std::vector<std::pair<uint8_t, double>> distances(k,
+	                                                  {0, std::numeric_limits<double>::max()});
+
+	// For each data point in the training data, calculate the Euclidean distance.
+	// Could be reduced using optimizations. See train() function.
+	for (size_t i = 0; i < training_data->size(); ++i) {
+		// Calculate the Euclidean distance between the test data point and the training
+		// data point.
+		double distance = 0.0;
+		for (const auto &feature: view.get_features().get_features()) {
+			double diff =
+			        view.feature(index, feature) - training_data->feature(i, feature);
+			distance += diff * diff;
+		}
+
+		// Find the element pointer of the maximum distance in the distances vector.
+		auto max_distance = std::max_element(
+		        distances.begin(), distances.end(),
+		        [](const auto &lhs, const auto &rhs) { return lhs.second < rhs.second; });
+
+		// If the current distance is less than the maximum distance, replace the maximum
+		// distance.
+		if (distance < max_distance->second) {
+			max_distance->first = training_data->label(i);
+			max_distance->second = distance;
+		}
+	}
+
+	// Return the most common label among the k-nearest neighbors.
+	std::unordered_map<uint8_t, size_t> label_counts;
+
+	for (const auto &[label, _]: distances) label_counts[label]++;
+
+	auto most_common_label = std::max_element(
+	        label_counts.begin(), label_counts.end(),
+	        [](const auto &lhs, const auto &rhs) { return lhs.second < rhs.second; });
+
+	return most_common_label->first;
+}
+
+} // namespace classifiers
---
+++ src/classifiers/nearest_neighbor.h
@@ -0,0 +1,62 @@
+//
+// Created by Anshul Gupta on 5/24/24.
+//
+
+#ifndef CS170PROJECT2_NEAREST_NEIGHBOR_H
+#define CS170PROJECT2_NEAREST_NEIGHBOR_H
+
+#include <optional>
+
+#include "../classifier.h"
+
+namespace classifiers {
+
+/**
+ * @class nearest_neighbor
+ * @brief A class representing a nearest neighbor classifier.
+ *
+ * The nearest_neighbor class is a concrete implementation of the classifier abstract base class.
+ * It uses the k-nearest neighbor algorithm to classify data points based on their feature values.
+ */
+class nearest_neighbor : public classifier {
+public:
+	/**
+	 * @brief Construct a nearest_neighbor object with a specified value of k.
+	 *
+	 * The k parameter determines the number of neighbors used for classification.
+	 * It must be a positive odd integer.
+	 *
+	 * @param k The number of neighbors to consider for classification.
+	 *
+	 * @pre k > 0 and odd
+	 */
+	explicit nearest_neighbor(size_t k);
+
+	void train(data_view view) override;
+	[[nodiscard]] double test(data_view view) const override;
+
+private:
+	size_t k;
+	std::optional<data_view> training_data;
+
+	/**
+	 * @brief Predicts the label of a given data point based on its nearest neighbors.
+	 *
+	 * This function predicts the label of a given data point based on its k nearest neighbors.
+	 * It calculates the Euclidean distance between the test data point and each training data
+	 * point, and selects the k nearest neighbors with the lowest distances.
+	 * It then returns the most common label among these k nearest neighbors.
+	 *
+	 * @note This could be optimized by using k-d trees or vp-trees to reduce the number of
+	 * distance calculations.
+	 *
+	 * @param view The data view containing the data.
+	 * @param index The index of the data point in the view to be predicted.
+	 * @return The predicted label of the data point.
+	 */
+	[[nodiscard]] uint8_t predict(const data_view &view, size_t index) const;
+};
+
+} // namespace classifiers
+
+#endif //CS170PROJECT2_NEAREST_NEIGHBOR_H
